f5dff09a0744326c830ffd083a8aa45d90abf13d
Min Tang <z5449664@ad.unsw.edu.au>
2023-11-06 14:18:43 +1100

    Initial commit

--- lectures/lecture2.py
+++ lectures/lecture2.py
@@ -17,3 +17,11 @@ import math
 f = 0.2+0.2+0.2
 print(math.isclose(f,0.6))
 
+i = 1.0
+print(type(i))
+
+a = True
+b = 5
+
+print(f"The value of {a} and is {b}")
+print("The value of {} and is {}".format(a,b))
